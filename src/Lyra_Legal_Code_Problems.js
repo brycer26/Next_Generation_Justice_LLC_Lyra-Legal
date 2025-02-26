f35b17941bbeb2f0f667302dbf54fe0f67a429f4
rileyj3 <rileyj3@berea.edu>
2025-02-26 12:42:30 -0500

    Added comments to code and the final function (header).

--- src/Lyra_Legal_Code_Problems.js
+++ src/Lyra_Legal_Code_Problems.js
@@ -1,27 +1,24 @@
 function multiply_array(array){
-    let newArray = [];
+    let newArray = []; // new array to hold answer
 
-    for (let i =0; i < array.length; i++){
-        if (array[i] > 0) {
-            newArray.push(array[i] * 2);
+    for (let i =0; i < array.length; i++){ // iterates over the array length and progresses i over each element
+        if (array[i] > 0) { // if the element in array is greater than 0
+            newArray.push(array[i] * 2); // times that element by 2 and push it to the new array
         }
     }
-    return newArray;
+    return newArray; // return the answer, or newArray
 }
 
 
 
 function string_processor(sentence){
-    let newSentence = {}
-    let split = sentence.toLowerCase().split(" ");
+    let newSentence = {} // new dictionary to hold answer
+    let split = sentence.toLowerCase().split(" "); // turning passed through sentence into lowercase and splitting it by each word
 
 
-    for (let sent of split){
-        if(sent) {
-            newSentence[sent] = (newSentence[sent] || 0) + 1
-        }
-        else {
-            newSentence[sent] = 1;
+    for (let sent of split){ // iterates through each word in split
+        if(sent) { // if split is not empty
+            newSentence[sent] = (newSentence[sent] || 0) + 1 // takes each word(sent) and initializes it to 0 if not used before and adds 1; If used before, adds 1.
         }
     }
     return newSentence;
@@ -35,21 +32,31 @@ function average_grades(students){
 
     students.forEach(function(student) { //iterates over each student in the array of students
         for (let subject in student.grades){
-            if (!(subject in gradesum)){ //if subject is not in gradesum, set to 0 and add, otherwise just add
+            if (!(subject in gradesum)){ //if subject is not in gradesum, it sets it to 0 and keeps the value
                 gradesum[subject] = 0;
                 subjectcount[subject] = 0;
             }
-            gradesum[subject] += student.grades[subject];
-            subjectcount[subject]++;
+            gradesum[subject] += student.grades[subject]; // adds the grade of each subjet to the gradesum counter
+            subjectcount[subject]++; // iterates through the subjects and keeps track of how many there are
         }
     });
 
-    let averagegrade = {};
-    for (let subject in gradesum){
-        averagegrade[subject] = gradesum[subject] / subjectcount[subject];
+    let averagegrade = {}; // dictionary to hold the average for each subject
+    for (let subject in gradesum){ // each subject in the gradesum count
+        averagegrade[subject] = gradesum[subject] / subjectcount[subject]; // takes the total gradesum and divides it by total subjectcount to get average grade
     }
-    return averagegrade;
+    return averagegrade; // returns the answer, or the average grade
 }
+
+
+
+
+async function user_data(userID, timeout = 3000){
+    // I have no clue where to even start this function at !! I am not experienced with async functions or retrieving an API in them.
+    // I would not want to attempt to do this problem with the help of AI since I have no idea and give off any type of false hope, so I hope this is understandable! :)
+}
+
+
 
 
 let array = [1, 2, 3, 4, 5, -6, -7, -8,-9, 10]; // this is the array passed into my array_multiplication function.
